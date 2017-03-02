63e5d178924b35e26ea0e1e9d54ddce0adbb5cba
simon <simon@DESKTOP>
2017-03-02 10:17:33 +0100

    Added index view

--- src/comic-book-gallery/Data/ComicBookRepository.cs
+++ src/comic-book-gallery/Data/ComicBookRepository.cs
@@ -10,55 +10,55 @@ namespace comic_book_gallery.Data
     {
 
         private static ComicBook[] _comicBooks = new ComicBook[]
-  {
-    new ComicBook()
-    {
-        SeriesTitle = "The Amazing Spider-Man",
-        IssueNumber = 700,
-        DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
-        Id = 1,
-        Artists = new Artist[]
-        {
-            new Artist() { Name = "Dan Slott", Role = "Script" },
-            new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-            new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-            new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-            new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
-        },
-        Favorite = false
-    },
-    new ComicBook()
-    {
-        SeriesTitle = "The Amazing Spider-Man",
-        IssueNumber = 657,
-        DescriptionHTML = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
-        Id = 2,
-        Artists = new Artist[]
-        {
-            new Artist() { Name = "Dan Slott", Role = "Script" },
-            new Artist() { Name = "Marcos Martin", Role = "Pencils" },
-            new Artist() { Name = "Marcos Martin", Role = "Inks" },
-            new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
-            new Artist() { Name = "Joe Caramagna", Role = "Letters" }
-        },
-        Favorite = false
-    },
-    new ComicBook()
-    {
-        SeriesTitle = "Bone",
-        IssueNumber = 50,
-        DescriptionHTML = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
-        Id = 3,
-        Artists = new Artist[]
-        {
-            new Artist() { Name = "Jeff Smith", Role = "Script" },
-            new Artist() { Name = "Jeff Smith", Role = "Pencils" },
-            new Artist() { Name = "Jeff Smith", Role = "Inks" },
-            new Artist() { Name = "Jeff Smith", Role = "Letters" }
-        },
-        Favorite = false
-    }
-  };
+          {
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
+                Id = 1,
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
+                },
+                Favorite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHTML = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Id = 2,
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+                    new Artist() { Name = "Marcos Martin", Role = "Inks" },
+                    new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+                    new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+                },
+                Favorite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHTML = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                Id = 3,
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Jeff Smith", Role = "Script" },
+                    new Artist() { Name = "Jeff Smith", Role = "Pencils" },
+                    new Artist() { Name = "Jeff Smith", Role = "Inks" },
+                    new Artist() { Name = "Jeff Smith", Role = "Letters" }
+                },
+                Favorite = false
+            }
+          };
 
         public ComicBook getComicBook(int id)
         {
@@ -66,7 +66,7 @@ namespace comic_book_gallery.Data
 
             foreach (var comicBook in _comicBooks)
             {
-                if(comicBook.Id == id)
+                if (comicBook.Id == id)
                 {
                     comicBookToReturn = comicBook;
                     break;
@@ -74,6 +74,11 @@ namespace comic_book_gallery.Data
             }
 
             return comicBookToReturn;
+        }
+
+        public ComicBook[] getComicBooks()
+        {
+            return _comicBooks;
         }
     }
 }
